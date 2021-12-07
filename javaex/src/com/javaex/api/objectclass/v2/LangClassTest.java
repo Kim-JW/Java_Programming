2087cdbd4512b74d3e6d36c852973005cae9c959
BIT <BIT@DESKTOP-F1UMR2K>
2021-12-07 15:04:22 +0900

    Java API & Override toString(), equals() & clone, Object Copy

---
+++ javaex/src/com/javaex/api/objectclass/v2/LangClassTest.java
@@ -0,0 +1,18 @@
+package com.javaex.api.objectclass.v2;
+
+public class LangClassTest {
+
+	public static void main(String[] args) {
+		Point p = new Point(10, 10);
+		Point p2 = new Point(10, 10);
+		String s = "g";
+		String s2 = new String("g");
+		
+		System.out.println(s.equals(s2));
+		System.out.println();
+		
+		System.out.println("p==p2 ? " + (p==p2));
+		System.out.println("p.equals(p2) ? " + p.equals(p2));
+	}
+
+}
